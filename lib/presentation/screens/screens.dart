6274249e72595516458ab7659fa70e00069be8ae
RoberSerr <robertoserrano@robertoserrano.es>
2023-04-22 11:27:04 +0200

    Creación de  ButtonsScreen y CardsScreen

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,4 +1,7 @@
 
 
+
+export 'package:flutter_app_widgets/presentation/screens/buttons/buttons_screen.dart';
+export 'package:flutter_app_widgets/presentation/screens/cards/cards_screen.dart';
 export 'package:flutter_app_widgets/presentation/screens/home/home_screen.dart';
 
