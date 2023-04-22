2fc0ac4238bc6b6381ee1ed1527405bfc98c8a2d
RoberSerr <robertoserrano@robertoserrano.es>
2023-04-22 10:53:52 +0200

    Modificados estilos AppBar

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -28,7 +28,12 @@ class AppTheme {
   ThemeData getTheme() => ThemeData(
 
     useMaterial3: true,
-    colorSchemeSeed: colorList[ selectedColor ]
+    colorSchemeSeed: colorList[ selectedColor ],
+
+    // AppBar
+    appBarTheme: const AppBarTheme(
+      centerTitle: true
+    )
 
   );
 
